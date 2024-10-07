f361f5ebd0d1ae5992f68d93f8a67fba661a8cf4
Mihai Indreias <mihai.indreias@epfl.ch>
2024-10-07 22:33:03 +0200

    fix: create equals and hashCode methods for Trip class

--- app/src/main/java/com/android/voyageur/model/trip/Trip.kt
+++ app/src/main/java/com/android/voyageur/model/trip/Trip.kt
@@ -13,4 +13,15 @@ data class Trip(
     val startDate: Timestamp,
     val endDate: Timestamp,
     val activities: Array<Any>, // TODO : replace this with activity model
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Trip
+
+        return id == other.id
+    }
+
+    override fun hashCode(): Int = id.hashCode()
+}
