d1019a144545477c89b7890034eddf14c838be7a
eleonoraka <100470953+eleonoraka@users.noreply.github.com>
2024-10-10 15:11:04 +0200

    style: reformat code

--- app/src/androidTest/java/com/android/voyageur/ui/authentication/LogInTest.kt
+++ app/src/androidTest/java/com/android/voyageur/ui/authentication/LogInTest.kt
@@ -4,10 +4,7 @@ import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithTag
-import androidx.compose.ui.test.performClick
 import androidx.test.espresso.intent.Intents
-import androidx.test.espresso.intent.Intents.intended
-import androidx.test.espresso.intent.matcher.IntentMatchers.toPackage
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.android.voyageur.MainActivity
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
