8ccb6d337e151b2ecec8fd06a45d7c7c46d112fc
mirceagavrizi <mirceagavrizi@gmail.com>
2024-10-16 17:12:17 +0200

    Added tests for AddTrip
    - added 3 new tests for the AddTrip Screen
    - 2 of them check the convertion to timestamps
    1 of them checks that the trip is not saved with an empty title

--- app/src/androidTest/java/com/android/voyageur/ui/overview/AddTripTest.kt
+++ app/src/androidTest/java/com/android/voyageur/ui/overview/AddTripTest.kt
@@ -1,5 +1,7 @@
 package com.android.voyageur.ui.overview
 
+import android.icu.util.GregorianCalendar
+import android.icu.util.TimeZone
 import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
 import com.android.voyageur.model.trip.TripRepository
@@ -7,6 +9,10 @@ import com.android.voyageur.model.trip.TripType
 import com.android.voyageur.model.trip.TripsViewModel
 import com.android.voyageur.ui.navigation.NavigationActions
 import com.android.voyageur.ui.navigation.Screen
+import com.google.firebase.Timestamp
+import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertNotNull
+import org.junit.Assert.assertNull
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -66,11 +72,29 @@ class AddTripScreenTest {
   }
 
   @Test
-  fun doesNotSubmitWithInvalidDate() {
+  fun addTripScreen_doesNotSubmitWithInvalidDate() {
     composeTestRule.setContent { AddTripScreen(tripsViewModel, navigationActions) }
 
     composeTestRule.onNodeWithTag("inputStartDate").performTextClearance()
     composeTestRule.onNodeWithTag("inputStartDate").performTextInput("notadate")
+
+    composeTestRule.onNodeWithTag("inputEndDate").performTextClearance()
+    composeTestRule.onNodeWithTag("inputEndDate").performTextInput("05/01/2024")
+
+    composeTestRule.onNodeWithTag("tripSave").performClick()
+
+    verify(tripRepository, never()).updateTrip(anyNonNull(), anyNonNull(), anyNonNull())
+  }
+
+  @Test
+  fun addTripScreen_doesNotSubmitWithInvalidTitle() {
+    composeTestRule.setContent { AddTripScreen(tripsViewModel, navigationActions) }
+
+    composeTestRule.onNodeWithTag("inputTripTitle").performTextClearance()
+    composeTestRule.onNodeWithTag("inputTripTitle").performTextInput("")
+
+    composeTestRule.onNodeWithTag("inputStartDate").performTextClearance()
+    composeTestRule.onNodeWithTag("inputStartDate").performTextInput("01/01/2024")
 
     composeTestRule.onNodeWithTag("inputEndDate").performTextClearance()
     composeTestRule.onNodeWithTag("inputEndDate").performTextInput("05/01/2024")
@@ -89,5 +113,44 @@ class AddTripScreenTest {
 
     composeTestRule.onNodeWithTag("tripTypeTourism").performClick()
     assert(tripsViewModel.tripType.value == TripType.TOURISM)
+  }
+
+  private fun convertToTimestamp(dateString: String): Timestamp? {
+    val calendar = GregorianCalendar(TimeZone.getTimeZone("UTC"))
+    val dateParts = dateString.split("/")
+    return if (dateParts.size == 3) {
+      try {
+        calendar.set(dateParts[2].toInt(), dateParts[1].toInt() - 1, dateParts[0].toInt(), 0, 0, 0)
+        calendar.set(GregorianCalendar.MILLISECOND, 0)
+        Timestamp(calendar.time)
+      } catch (e: NumberFormatException) {
+        null
+      }
+    } else {
+      null
+    }
+  }
+
+  @Test
+  fun convertToTimestamp_validDate() {
+    val dateString = "01/01/2024"
+
+    val utcCalendar = GregorianCalendar(TimeZone.getTimeZone("UTC"))
+    utcCalendar.set(2024, 0, 1, 0, 0, 0)
+    utcCalendar.set(GregorianCalendar.MILLISECOND, 0)
+    val expectedTimestamp = Timestamp(utcCalendar.time)
+
+    val result = convertToTimestamp(dateString)
+
+    assertNotNull(result)
+    assertEquals(expectedTimestamp.seconds, result?.seconds)
+  }
+
+  @Test
+  fun convertToTimestamp_invalidDate() {
+    val dateString = "invalid"
+    val result = convertToTimestamp(dateString)
+
+    assertNull(result)
   }
 }
