77c276454cb2014eed924bfaa48f43b8ade2258c
1andreea26 <60853926+1andreea26@users.noreply.github.com>
2024-10-31 12:09:52 +0100

    UI: Add correct dates

--- app/src/main/java/com/android/voyageur/ui/trip/schedule/ByDay.kt
+++ app/src/main/java/com/android/voyageur/ui/trip/schedule/ByDay.kt
@@ -1,7 +1,20 @@
 package com.android.voyageur.ui.trip.schedule
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ArrowBack
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -9,16 +22,31 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.rememberAsyncImagePainter
+import com.android.voyageur.R
+import com.android.voyageur.model.trip.Trip
 import com.android.voyageur.model.trip.TripsViewModel
 import com.android.voyageur.ui.navigation.BottomNavigationMenu
 import com.android.voyageur.ui.navigation.LIST_TRIP_LEVEL_DESTINATION
 import com.android.voyageur.ui.navigation.NavigationActions
 import com.android.voyageur.ui.navigation.Screen
+import com.android.voyageur.ui.overview.toDateString
+import com.google.firebase.Timestamp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -33,16 +61,18 @@ fun ByDayScreen(
       // TODO: Final implementation of ByDayScreen
       modifier = Modifier.testTag("byDayScreen"),
       topBar = {
-        TopAppBar(
-            modifier = Modifier.testTag("topBar"),
-            title = { Text("Schedule ByDay:") },
-            navigationIcon = {
-              IconButton(
-                  modifier = Modifier.testTag("backToOverviewButton"),
-                  onClick = { navigationActions.navigateTo(Screen.OVERVIEW) }) {
-                    Icon(imageVector = Icons.Outlined.Home, contentDescription = "Home")
-                  }
-            })
+          TopBarWithImage(selectedTrip = trip, navigationActions = navigationActions)
+         /**   TopAppBar(
+                modifier = Modifier.testTag("topBar"),
+                title = { Text("Schedule ByDay:") },
+                navigationIcon = {
+                  IconButton(
+                      modifier = Modifier.testTag("backToOverviewButton"),
+                      onClick = { navigationActions.navigateTo(Screen.OVERVIEW) }) {
+                        Icon(imageVector = Icons.Outlined.Home, contentDescription = "Home")
+                      }
+                })
+         */
       },
       bottomBar = {
         BottomNavigationMenu(
@@ -52,8 +82,11 @@ fun ByDayScreen(
       },
       content = { pd ->
         Text(
-            modifier = Modifier.padding(pd).testTag("emptyByDayPrompt"),
+            modifier = Modifier
+                .padding(pd)
+                .testTag("emptyByDayPrompt"),
             text =
                 "You're viewing the ByDay screen for ${trip.name}, but it's not implemented yet.")
       })
 }
+
