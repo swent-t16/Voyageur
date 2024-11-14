b5098d3acfaad5d481fb1dab46b8b331dd32655b
eleonoraka <100470953+eleonoraka@users.noreply.github.com>
2024-11-14 21:46:34 +0100

    style: optimize imports

--- app/src/main/java/com/android/voyageur/ui/search/SearchScreen.kt
+++ app/src/main/java/com/android/voyageur/ui/search/SearchScreen.kt
@@ -17,7 +17,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
