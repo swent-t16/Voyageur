00ca6701366e738d9599d4b175dc87720d88e1ac
mirceagavrizi <mirceagavrizi@gmail.com>
2024-11-28 23:35:03 +0100

    Update Trip Class Methods + tests

--- app/src/main/java/com/android/voyageur/model/trip/Trip.kt
+++ app/src/main/java/com/android/voyageur/model/trip/Trip.kt
@@ -23,9 +23,37 @@ data class Trip(
   val tripType: TripType
     get() = TripType.valueOf(type.toString())
 
-  override fun equals(other: Any?): Boolean = other is Trip && id == other.id
+  override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (other !is Trip) return false
 
-  override fun hashCode(): Int = id.hashCode()
+    return id == other.id &&
+        creator == other.creator &&
+        participants == other.participants &&
+        description == other.description &&
+        name == other.name &&
+        locations == other.locations &&
+        startDate == other.startDate &&
+        endDate == other.endDate &&
+        activities == other.activities &&
+        type == other.type &&
+        imageUri == other.imageUri
+  }
+
+  override fun hashCode(): Int {
+    var result = id.hashCode()
+    result = 31 * result + creator.hashCode()
+    result = 31 * result + participants.hashCode()
+    result = 31 * result + description.hashCode()
+    result = 31 * result + name.hashCode()
+    result = 31 * result + locations.hashCode()
+    result = 31 * result + startDate.hashCode()
+    result = 31 * result + endDate.hashCode()
+    result = 31 * result + activities.hashCode()
+    result = 31 * result + type.hashCode()
+    result = 31 * result + imageUri.hashCode()
+    return result
+  }
 }
 
 enum class TripType {
