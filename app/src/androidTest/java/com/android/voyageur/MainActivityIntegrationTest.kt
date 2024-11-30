9cb20084b32c48ae01325edccc7d08c005f73d52
BogdanCDev <bogdan.cirje@epfl.ch>
2024-11-30 02:47:02 +0100

    add tests

--- app/src/androidTest/java/com/android/voyageur/MainActivityIntegrationTest.kt
+++ app/src/androidTest/java/com/android/voyageur/MainActivityIntegrationTest.kt
@@ -1,12 +1,10 @@
-package com.android.voyageur.ui
+package com.android.voyageur
 
 import android.app.NotificationManager
 import android.content.Context
 import android.content.Intent
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.android.voyageur.MainActivity
-import com.android.voyageur.R
 import com.android.voyageur.ui.notifications.NotificationHelper
 import org.junit.After
 import org.junit.Assert.fail
---
+++ app/src/androidTest/java/com/android/voyageur/MainActivityTestPermission.kt
@@ -0,0 +1,29 @@
+package com.android.voyageur
+
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import org.junit.Assert.assertEquals
+import android.Manifest
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTestPermission {
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun testNotificationPermissionRequestOnAndroid13() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            val scenario = activityRule.scenario
+            scenario.onActivity { activity ->
+                val permissionRequested = activity.checkSelfPermission(Manifest.permission.POST_NOTIFICATIONS)
+                assertEquals(PackageManager.PERMISSION_DENIED, permissionRequested)
+            }
+        }
+    }
+}
