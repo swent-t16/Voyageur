0fdae07293a405b09da0987e9bea77fdf95c60c6
BogdanCDev <bogdan.cirje@epfl.ch>
2024-11-30 03:02:59 +0100

    add tests

--- app/src/androidTest/java/com/android/voyageur/MainActivityTestPermission.kt
+++ app/src/androidTest/java/com/android/voyageur/MainActivityTestPermission.kt
@@ -1,10 +1,23 @@
 package com.android.voyageur
 
 import android.Manifest
+import android.app.NotificationManager
+import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Build
+import android.view.View
+import android.widget.TextView
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.collectAsState
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.android.voyageur.ui.notifications.NotificationHelper
+import com.android.voyageur.utils.ConnectionState
+import com.android.voyageur.utils.connectivityState
+import junit.framework.Assert.assertNotNull
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
 import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
@@ -13,7 +26,8 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class MainActivityTestPermission {
 
-  @get:Rule val activityRule = ActivityScenarioRule(MainActivity::class.java)
+  @get:Rule
+  val activityRule = ActivityScenarioRule(MainActivity::class.java)
 
   @Test
   fun testNotificationPermissionRequestOnAndroid13() {
@@ -21,9 +35,28 @@ class MainActivityTestPermission {
       val scenario = activityRule.scenario
       scenario.onActivity { activity ->
         val permissionRequested =
-            activity.checkSelfPermission(Manifest.permission.POST_NOTIFICATIONS)
+          activity.checkSelfPermission(Manifest.permission.POST_NOTIFICATIONS)
         assertEquals(PackageManager.PERMISSION_DENIED, permissionRequested)
       }
     }
   }
+
+  @Test
+  fun testNotificationPermissionDeniedBehavior() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+      val scenario = activityRule.scenario
+      scenario.onActivity { activity ->
+        activity.onRequestPermissionsResult(
+          100,
+          arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+          intArrayOf(PackageManager.PERMISSION_DENIED)
+        )
+        assertEquals(
+          PackageManager.PERMISSION_DENIED,
+          activity.checkSelfPermission(Manifest.permission.POST_NOTIFICATIONS)
+        )
+      }
+    }
+  }
+
 }
