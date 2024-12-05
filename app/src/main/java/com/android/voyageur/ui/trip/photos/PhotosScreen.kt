80ec5afe4296630043d239c5b277892cbcd2cc6e
mirceagavrizi <mirceagavrizi@gmail.com>
2024-12-06 00:21:12 +0100

    update UI of PhotosScreen
    - display photos 4 per row instead of in a column

--- app/src/main/java/com/android/voyageur/ui/trip/photos/PhotosScreen.kt
+++ app/src/main/java/com/android/voyageur/ui/trip/photos/PhotosScreen.kt
@@ -4,19 +4,19 @@ import android.annotation.SuppressLint
 import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Download
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Card
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -33,6 +33,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
@@ -58,6 +59,7 @@ fun PhotosScreen(
 ) {
   val trip by tripsViewModel.selectedTrip.collectAsState()
   val photos = trip?.photos ?: emptyList()
+  var selectedPhoto by remember { mutableStateOf<String?>(null) }
   val context = LocalContext.current
   val status by connectivityState()
   val isConnected = status === ConnectionState.Available
@@ -100,71 +102,134 @@ fun PhotosScreen(
             )
           }
         } else {
-          LazyColumn(
-              verticalArrangement = Arrangement.spacedBy(10.dp, Alignment.Top),
-              horizontalAlignment = Alignment.CenterHorizontally,
-              modifier = Modifier.padding(pd).padding(top = 16.dp, bottom = 60.dp).fillMaxSize().testTag("lazyColumn")) {
-                photos.forEach { photo ->
-                  item {
-                    PhotoItem(photoUri = photo, tripsViewModel = tripsViewModel)
-                    Spacer(modifier = Modifier.height(10.dp))
-                  }
+          LazyVerticalGrid(
+              columns = GridCells.Fixed(4),
+              contentPadding = PaddingValues(8.dp),
+              verticalArrangement = Arrangement.spacedBy(8.dp),
+              horizontalArrangement = Arrangement.spacedBy(8.dp),
+              modifier =
+                  Modifier.padding(pd)
+                      .padding(top = 16.dp, bottom = 60.dp)
+                      .fillMaxSize()
+                      .testTag("lazyVerticalGrid")) {
+                items(photos.size) { index ->
+                  val photoUri = photos[index]
+                  PhotoThumbnail(photoUri) { selectedPhoto = photoUri }
                 }
               }
+          selectedPhoto?.let { photoUri ->
+            val initialIndex = photos.indexOf(photoUri)
+            PhotoDialog(
+                photoUri = photoUri,
+                photoList = photos,
+                onDismiss = { selectedPhoto = null },
+                initialIndex = initialIndex,
+                tripsViewModel = tripsViewModel)
+          }
         }
       })
 }
 
+@Composable
+fun PhotoThumbnail(photoUri: String, onClick: () -> Unit) {
+  Box(
+      Modifier.size(80.dp)
+          .background(MaterialTheme.colorScheme.background, RoundedCornerShape(4.dp))
+          .clickable { onClick() },
+      contentAlignment = Alignment.Center) {
+        Image(
+            painter = rememberAsyncImagePainter(photoUri),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize())
+      }
+}
+
 @OptIn(ExperimentalCoroutinesApi::class)
 @Composable
-fun PhotoItem(photoUri: String, tripsViewModel: TripsViewModel) {
-  var isExpanded by remember { mutableStateOf(false) }
-  var showDialog by remember { mutableStateOf(false) }
+fun PhotoDialog(
+    photoUri: String,
+    photoList: List<String>,
+    onDismiss: () -> Unit,
+    initialIndex: Int,
+    tripsViewModel: TripsViewModel
+) {
+  var currentIndex by remember { mutableStateOf(initialIndex) }
+  val currentPhoto = photoList.getOrNull(currentIndex)
   val context = LocalContext.current
+  var showDialog by remember { mutableStateOf(false) }
   val status by connectivityState()
   val isConnected = status === ConnectionState.Available
-  Card(shape = RoundedCornerShape(8.dp), modifier = Modifier.width(360.dp).padding(start = 10.dp, top = 10.dp, end = 10.dp, bottom = 10.dp).fillMaxWidth()) {
-      Box(modifier = Modifier.fillMaxWidth()) {
-          Image(
-              painter = rememberAsyncImagePainter(photoUri),
-              contentDescription = "Trip Photo",
-              contentScale = ContentScale.Crop,
-              modifier = Modifier.fillMaxWidth().height(200.dp)
-          )
-          Box(
-              modifier = Modifier
-                  .align(Alignment.TopEnd)
-                  .padding(8.dp)
-          ) {
-              IconButton(
-                  enabled = isConnected,
-                  onClick = { isExpanded = !isExpanded },
-                  modifier = Modifier.align(Alignment.TopEnd).padding(8.dp)
-                      .testTag("expandIcon_${photoUri}")
-              ) {
-                  Icon(
-                      imageVector = Icons.Default.MoreVert,
-                      contentDescription = if (isExpanded) "Collapse" else "Expand"
-                  )
-              }
-              DropdownMenu(
-                  expanded = isExpanded,
-                  onDismissRequest = { isExpanded = false },
-                  modifier = Modifier.background(MaterialTheme.colorScheme.secondaryContainer)
-              ) {
-                  DropdownMenuItem(
+
+  Box(
+      modifier =
+          Modifier.fillMaxSize().background(Color.Black.copy(alpha = 0.8f)).clickable {
+            onDismiss()
+          },
+      contentAlignment = Alignment.Center) {
+        currentPhoto?.let {
+          Column(
+              horizontalAlignment = Alignment.CenterHorizontally,
+              verticalArrangement = Arrangement.Center,
+              modifier = Modifier.wrapContentSize()) {
+                Box(modifier = Modifier.fillMaxWidth().wrapContentSize(align = Alignment.Center)) {
+                  // Image
+                  Image(
+                      painter = rememberAsyncImagePainter(it),
+                      contentDescription = "Full-size photo",
+                      contentScale = ContentScale.Fit,
+                      modifier = Modifier.fillMaxWidth().padding(16.dp).clickable {})
+
+                  // Row to position the left and right buttons at the middle of the height
+                  Row(
+                      horizontalArrangement = Arrangement.SpaceBetween,
+                      verticalAlignment = Alignment.CenterVertically,
+                      modifier =
+                          Modifier.fillMaxWidth()
+                              .align(Alignment.Center) // Center the Row vertically
+                      ) {
+                        // Left Button ("<")
+                        IconButton(
+                            onClick = {
+                              currentIndex = (currentIndex - 1 + photoList.size) % photoList.size
+                            },
+                            enabled = photoList.size > 1) {
+                              Text("<", color = Color.White)
+                            }
+
+                        // Right Button (">")
+                        IconButton(
+                            onClick = { currentIndex = (currentIndex + 1) % photoList.size },
+                            enabled = photoList.size > 1) {
+                              Text(">", color = Color.White)
+                            }
+                      }
+                  // Delete button (bottom-right corner)
+                  IconButton(
+                      onClick = { showDialog = true },
+                      modifier = Modifier.align(Alignment.BottomEnd).padding(16.dp)) {
+                        Icon(
+                            imageVector = Icons.Default.Delete,
+                            contentDescription = "Delete Photo",
+                            tint = Color.White)
+                      }
+
+                  // Download button (top-right corner)
+                  IconButton(
                       onClick = {
-                          isExpanded = false
-                          showDialog = true
+                        // TODO: implement photo downloading functionality
+                        Toast.makeText(context, "Photo downloaded", Toast.LENGTH_SHORT).show()
                       },
-                      text = { Text("Delete") },
-                      modifier = Modifier.testTag("deleteMenuItem_${photoUri}")
-                  )
+                      modifier = Modifier.align(Alignment.TopEnd).padding(16.dp)) {
+                        Icon(
+                            imageVector = Icons.Default.Download,
+                            contentDescription = "Download Photo",
+                            tint = Color.White)
+                      }
+                }
               }
-          }
+        }
       }
-  }
-  // Confirmation Dialog
   if (showDialog) {
     AlertDialog(
         onDismissRequest = { showDialog = false },
@@ -175,6 +240,7 @@ fun PhotoItem(photoUri: String, tripsViewModel: TripsViewModel) {
               onClick = {
                 if (isConnected) {
                   tripsViewModel.removePhotoFromTrip(photoUri)
+                  onDismiss()
                   Toast.makeText(context, "Photo successfully deleted", Toast.LENGTH_SHORT).show()
                 } else {
                   Toast.makeText(context, "No internet connection", Toast.LENGTH_SHORT).show()
