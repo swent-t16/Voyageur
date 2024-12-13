c60c839d76ebb90718a2b223c950aa8aeea6bbcf
BogdanCDev <bogdan.cirje@epfl.ch>
2024-12-13 01:15:52 +0100

    fix unique notification id

--- app/src/main/java/com/android/voyageur/ui/notifications/AndroidNotificationProvider.kt
+++ app/src/main/java/com/android/voyageur/ui/notifications/AndroidNotificationProvider.kt
@@ -5,23 +5,25 @@ import androidx.core.app.NotificationCompat
 import com.android.voyageur.R
 
 open class AndroidNotificationProvider(private val context: Context) : NotificationProvider {
-  override fun showNewFriendRequestNotification(senderName: String) {
-    NotificationHelper.showNotification(
-        context = context,
-        notificationId = 1001, // Can be any unique ID
-        title = context.getString(R.string.new_friend_request),
-        text = context.getString(R.string.friend_request_message, senderName),
-        iconResId = R.drawable.app_logo,
-        priority = NotificationCompat.PRIORITY_HIGH)
-  }
+    override fun showNewFriendRequestNotification(senderName: String) {
+        val notificationId = System.currentTimeMillis().toInt()
+        NotificationHelper.showNotification(
+            context = context,
+            notificationId = notificationId,
+            title = context.getString(R.string.new_friend_request),
+            text = context.getString(R.string.friend_request_message, senderName),
+            iconResId = R.drawable.app_logo,
+            priority = NotificationCompat.PRIORITY_HIGH)
+    }
 
-  override fun showFriendRequestAcceptedNotification(acceptorName: String) {
-    NotificationHelper.showNotification(
-        context = context,
-        notificationId = 1002, // Can be any unique ID
-        title = context.getString(R.string.friend_request_accepted),
-        text = context.getString(R.string.friend_request_accepted_message, acceptorName),
-        iconResId = R.drawable.app_logo,
-        priority = NotificationCompat.PRIORITY_HIGH)
-  }
+    override fun showFriendRequestAcceptedNotification(acceptorName: String) {
+        val notificationId = System.currentTimeMillis().toInt()
+        NotificationHelper.showNotification(
+            context = context,
+            notificationId = notificationId,
+            title = context.getString(R.string.friend_request_accepted),
+            text = context.getString(R.string.friend_request_accepted_message, acceptorName),
+            iconResId = R.drawable.app_logo,
+            priority = NotificationCompat.PRIORITY_HIGH)
+    }
 }
