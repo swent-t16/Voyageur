7f7b5896893d663da7d6bf5ab9a9aab4f83c8c28
1andreea26 <60853926+1andreea26@users.noreply.github.com>
2024-12-17 13:20:32 +0100

    feat: add button instead of checkbox.

--- app/src/main/java/com/android/voyageur/ui/formFields/UserDropdown.kt
+++ app/src/main/java/com/android/voyageur/ui/formFields/UserDropdown.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
@@ -99,9 +100,10 @@ fun UserDropdown(
             DropdownMenuItem(
                 text = { Text("${userPair.first.name} ") },
                 onClick = { onUpdate(userPair, index) },
-                leadingIcon = {
-                  Checkbox(
-                      checked = userPair.second, onCheckedChange = { onUpdate(userPair, index) })
+                trailingIcon = {
+                    Button(onClick = {}) {Text("send request") }
+                  //Checkbox(
+                  //    checked = userPair.second, onCheckedChange = { onUpdate(userPair, index) })
                 })
           }
         }
